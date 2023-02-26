736bdc4abf761490f859e9035264591a016bb54c
666774ak <118664667+666774ak@users.noreply.github.com>
2023-02-26 20:45:02 +0500

    26.02. 20.44

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,9 @@
 let loginButton = document.querySelector('.main-button');
 let loginPopUp = document.querySelector('.login-popup');
+let closeButton = document.querySelector('.close-btn')
 
 loginButton.addEventListener('click', showPopUp);
+closeButton.addEventListener('click', closePopUp);
 
 function showPopUp(evt) {
 console.log(evt);
@@ -9,3 +11,13 @@ evt.preventDefault();
 
 loginPopUp.classList.toggle('show-popup');
 };
+
+function closePopUp() {
+loginPopUp.classList.remove('show-popup')
+};
+
+loginPopUp.addEventListener('click', (e) => {
+    if (e.target.classList.contains('show-popup')) {
+        closePopUp();
+    }
+})
